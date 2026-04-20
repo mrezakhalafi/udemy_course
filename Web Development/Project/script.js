08a24fa7cad85e3e8825a3cbbc76b601115bc03f
mrezakhalafi <mrezakhalafi@yahoo.com>
2026-04-20 16:54:36 +0700

    Web Development 14.7

--- Web Development/Project/script.js
+++ Web Development/Project/script.js
@@ -10,3 +10,15 @@ typeof(true);
 var myName = "Reza";
 var yourName = prompt("What is your name?");
 alert("My name is " + myName + ", welcome to my course " + yourName + "!");
+
+// 14.5
+
+var a = "3";
+var b = "8";
+
+var temp = a;
+a = b;
+b = temp;
+
+console.log("a is " + a);
+console.log("b is " + b);
