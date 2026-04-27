0755369d4216be573e0dd056030ef2d2fa212d3f
mrezakhalafi <mrezakhalafi@yahoo.com>
2026-04-27 15:50:24 +0700

    Web Development 14.11

--- Web Development/Project/script.js
+++ Web Development/Project/script.js
@@ -35,5 +35,12 @@ var name = "Reza";
 
 alert(message + "," + name + "!");
 
+// 14.11
 
+var name = "Reza";
+name.length;
+
+var tweet = prompt("Write your tweet below :");
+var tweetLength = tweet.length;
+alert("You already written " + tweetLength + " characters, you have " + (280 - tweetLength) + " characters left!");
 
