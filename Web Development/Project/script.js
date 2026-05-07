691f66e22525552a7b5b45aecbe54740fc37b446
mrezakhalafi <mrezakhalafi@yahoo.com>
2026-05-07 17:48:16 +0700

    Web Development 14.16

--- Web Development/Project/script.js
+++ Web Development/Project/script.js
@@ -71,3 +71,13 @@ alert("Hello, "+ firstLetter + restLetter);
 var dogAge = prompt("What's your dog age?");
 var humanAge = ((dogAge - 2) * 4) + 21;
 alert("Your dog is " + humanAge + " human years old.");
+
+// 14.16
+
+var x = 5;
+// x+1;
+// x++;
+
+// x+2;
+x += 2;
+alert(x);
